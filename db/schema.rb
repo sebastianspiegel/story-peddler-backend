1ab42ca18ffbb52067e325121733c09a8ab95066
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-08 12:10:29 -0400

    update to correct config and addition of user to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_162227) do
+ActiveRecord::Schema.define(version: 2021_04_08_160934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2021_02_17_162227) do
     t.string "title"
     t.string "genre"
     t.string "summary"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
