2ab29c62e15f23552f24df7945eb7d57c1b01868
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-08 11:59:01 -0400

    set up for cookies and sessions and update to use port 3001

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://agitated-darwin-7d6641.netlify.app'
+    # origins 'https://agitated-darwin-7d6641.netlify.app'
+    origins 'http://localhost:3000'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
