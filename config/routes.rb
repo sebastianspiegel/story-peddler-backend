371a3a97197f4e7d82b205ee72a986105b876289
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-08 12:15:15 -0400

    add users to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :plot_points
   resources :characters
   resources :stories
+  resources :users, only: [:create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
