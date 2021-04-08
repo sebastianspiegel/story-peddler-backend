12e69cb3d083b7abc7eab40bcec80625f6a75363
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-08 12:35:48 -0400

    add methods for session controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :plot_points
   resources :characters
   resources :stories
-  resources :users, only: [:create, :show, :index]
+  resources :users, only: [:create, :show, :index] 
+
+#   resources :users, only: [:create, :show, :index] do 
+#     resources :items, only: [:create, :show, :index, :destroy]
+#  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
