d6941375d873b5d1d4b47b251d3f221c5ae7b594
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-12 13:21:39 -0400

    fix scheme to include user_id for stories table and remove email from users table

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,5 +2,7 @@ class Story < ApplicationRecord
     has_many :characters, dependent: :destroy
     has_many :plot_points, dependent: :destroy
 
+    belongs_to :user 
+
     validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,13 +37,13 @@ ActiveRecord::Schema.define(version: 2021_04_08_160934) do
     t.string "title"
     t.string "genre"
     t.string "summary"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
