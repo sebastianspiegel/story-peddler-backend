a468e4a59acb458eb81d6a7ee3d9a3651745b1d0
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-12 15:14:02 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
 #     resources :items, only: [:create, :show, :index, :destroy]
 #  end
 
+  post '/login',    to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
