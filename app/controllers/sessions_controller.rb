85f878c46da110c4a9fd880abd0d8322698ef304
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-04-14 14:16:07 -0400

    uclean up sessions controller and helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,14 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(username: session_params[:username])
         if @user && @user.authenticate(session_params[:password])
-            login!
+            # login!
+            session[:user_id] = @user.id
             render json: {
                 logged_in: true,
                 user: @user
             }
         else
-            redner json: {
+            render json: {
                 status: 401,
                 errors: ['no such user, please try again']
             }
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
     end
 
     def is_logged_in?
-        if logged_in? && current_user
+        if !!session[:user_id] && current_user
             render json: {
                 logged_in: true,
                 user: current_user
@@ -32,6 +33,7 @@ class SessionsController < ApplicationController
 
     def destroy
         logout!
+        byebug
         render json: {
             status: 200,
             logged_out: true
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,41 @@
 class UsersController < ActionController::Base
+    # def index 
+    #     users = User.all 
+    #     render json: StorySerializer.new(stories)
+    # end
 
+    # def show
+    #     story = Story.find(params[:id])
+    #     render json: story.to_json
+    # end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: {message: "success"}
+        else
+            render json: {message: "failed to create"}
+        end
+    end
+
+    # def destroy
+    #     story = Story.find(params[:id])
+    #     story.destroy
+    #     render json: {message: "#{story.title} has been deleted"}
+    # end
+
+    # def update 
+    #     story = Story.find(params[:id])
+    #     if story.update(story_params)
+    #         render json: StorySerializer.new(story)
+    #     else
+    #         render json: {error: "failed to update"}
+    #     end
+    # end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
