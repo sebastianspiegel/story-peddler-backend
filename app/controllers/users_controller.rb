f51568d1df419528608ee7e8be5edee2a485e1d2
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-06-15 12:04:13 -0400

    user show json edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,12 @@ class UsersController < ApplicationController
 
     def show
         user = User.find(params[:id])
+        # render json: {
+        #     # user.to_json(include: :stories, except: :password_digest)
+        #     user: user,
+        #     stories: user.stories,
+        #     success: true
+        # }
         render json: user.to_json(include: :stories, except: :password_digest)
     end
 
